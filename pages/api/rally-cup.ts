b52b35fa6f0b850766b0898f769d1a1a6ad6e6bd
Kevin Yu <kevinyu567@gmail.com>
2022-09-03 18:56:15 -0700

    Global CORS for API routes
    https://vercel.com/guides/how-to-enable-cors#enabling-cors-in-a-next.js-app

--- pages/api/rally-cup.ts
+++ pages/api/rally-cup.ts
@@ -1,23 +1,9 @@
 import {NextApiRequest, NextApiResponse} from 'next';
 import {getRallyCupStandings} from '../../util/sheets';
-import cors from 'cors';
 
-
-const corsHandler = cors({origin: true});
-
-// https://github.com/vercel/next.js/blob/canary/examples/api-routes-cors/pages/api/cors.ts
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: Function) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, (result: any) => {
-            if (result instanceof Error) return reject(result);
-            return resolve(result);
-        })
-    })
-}
 
 // GET /api/rally-cup
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    await runMiddleware(req, res, corsHandler);
     const standings = await getRallyCupStandings();
     res.json(standings);
 }
